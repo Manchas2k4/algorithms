22cfa31ab79b01dcd4e23294919bb65f46ef764c
Pedro Perez <pperezm@tec.mx>
2020-06-25 23:55:33 -0500

    dp changed

---
+++ 03_dp/uva357/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+	private static long matriz[][] = new long[6][30001];
+	private static int monedas[] = {0, 1, 5, 10, 25, 50};
+	
+	public static void llenaTabla() {
+		for (int j = 0; j < matriz[0].length; j++) {
+			matriz[0][j] = 0;
+		}
+		for (int i = 0; i < matriz.length; i++) {
+			matriz[i][0] = 1;
+		}
+		for (int i = 1; i < matriz.length; i++) {
+			for (int j = 1; j < matriz[i].length; j++) {
+				if (monedas[i] > j) {
+					matriz[i][j] = matriz[i - 1][j];
+				} else if (monedas[i] <= j) {
+					matriz[i][j] = matriz[i - 1][j] + matriz[i][j - monedas[i]];
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		Scanner stdIn = new Scanner(System.in);
+		int j;
+		
+		llenaTabla();
+		while(stdIn.hasNextInt()) {
+			j = stdIn.nextInt();
+			if (matriz[5][j] == 1) {
+				System.out.println("There is only 1 way to produce " + j + " cents change.");
+			} else {
+				System.out.println("There are " + matriz[5][j] + " ways to produce " + j + " cents change.");
+			}
+		}
+	}
+}
